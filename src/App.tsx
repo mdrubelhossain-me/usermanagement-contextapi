dbd8d8b32657e97372ff6255ff3c412744eec8d9
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-05-02 23:53:12 +0600

    user created

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([
+    { id: 1, name: 'John Doe',},
+    { id: 2, name: 'Jane Doe',},
+    { id: 3, name: 'Jack Doe',},
+    { id: 4, name: 'Jill Doe',},
+    { id: 5, name: 'Joe Doe',},
+    { id: 6, name: 'Jim Doe',},
+    { id: 7, name: 'Judy Doe',},
+    { id: 8, name: 'Jake Doe',},
+    { id: 9, name: 'Jessie Doe',},
+    { id: 10, name: 'Jordan Doe',},
+  ])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div>
+
+   </div>
   )
 }
 
